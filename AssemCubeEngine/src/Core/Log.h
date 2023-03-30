acc8995ea8a9f47533e6178b6976e4e9d307e3bb
Veljko <mileticveljko99@gmail.com>
2023-03-30 10:23:15 +0200

    Added Premake

--- AssemCubeEngine/src/Core/Log.h
+++ AssemCubeEngine/src/Core/Log.h
@@ -21,14 +21,14 @@ namespace ac
 	};
 }
 
-#define AC_CORE_TRACE(...) ::ac::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define AC_CORE_INFO(...) ::ac::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define AC_CORE_WARN(...) ::ac::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define AC_CORE_ERROR(...) ::ac::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define AC_CORE_FATAL(...) ::ac::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define AC_CORE_TRACE(...)	::ac::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define AC_CORE_INFO(...)	::ac::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define AC_CORE_WARN(...)	::ac::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define AC_CORE_ERROR(...)	::ac::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define AC_CORE_FATAL(...)	::ac::Log::GetCoreLogger()->fatal(__VA_ARGS__)
 
-#define AC_TRACE(...) ::ac::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define AC_INFO(...) ::ac::Log::GetClientLogger()->info(__VA_ARGS__)
-#define AC_WARN(...) ::ac::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define AC_ERROR(...) ::ac::Log::GetClientLogger()->error(__VA_ARGS__)
-#define AC_FATAL(...) ::ac::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define AC_TRACE(...)		::ac::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define AC_INFO(...)		::ac::Log::GetClientLogger()->info(__VA_ARGS__)
+#define AC_WARN(...)		::ac::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define AC_ERROR(...)		::ac::Log::GetClientLogger()->error(__VA_ARGS__)
+#define AC_FATAL(...)		::ac::Log::GetClientLogger()->fatal(__VA_ARGS__)
---
+++ premake5.lua
@@ -0,0 +1,113 @@
+workspace "AssemCubeEngine"
+    architecture "x64"
+    startproject "AssemCube"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "AssemCubeEngine"
+    location "AssemCubeEngine"
+    kind "sharedLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "off"
+
+    targetdir("bin/" .. outputDir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputDir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        staticruntime "on"
+        systemversion "latest"
+
+        defines 
+        {
+            "AC_PLATFORM_WINDOWS",
+            "AC_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputDir .. "/AssemCube/\"")
+        }
+
+    filter "configurations:Debug"
+        defines "AC_DEBUG"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        defines "AC_RELEASE"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        defines "AC_DIST"
+        runtime "Release"
+        optimize "on"
+        
+project "AssemCube"
+    location "AssemCube"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "off"
+
+    targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "AssemCubeEngine/vendor/spdlog/include",
+        "AssemCubeEngine/src"
+    }
+
+    links
+    {
+        "AssemCubeEngine"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+        defines
+        {
+            "AC_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "AC_DEBUG"
+        runtime "Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "AC_RELEASE"
+        runtime "Release"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "AC_DIST"
+        runtime "Release"
+        optimize "On"
