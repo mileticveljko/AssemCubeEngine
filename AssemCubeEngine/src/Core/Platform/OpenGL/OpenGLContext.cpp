054757bf45d05766e8f29ca05db7d87dc13ab128
Veljko <mileticveljko99@gmail.com>
2023-04-01 00:38:19 +0200

    First Triangle!

--- AssemCubeEngine/src/Core/Platform/OpenGL/OpenGLContext.cpp
+++ AssemCubeEngine/src/Core/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,11 @@ namespace ac {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		AC_CORE_ASSERT(status, "Failed to initialize Glad!");
+	
+		AC_CORE_INFO("  OpenGL Info: ");
+		AC_CORE_INFO("  Vendor: {}", *glGetString(GL_VENDOR));
+		AC_CORE_INFO("  Renderer: {}", *glGetString(GL_RENDERER));
+		AC_CORE_INFO("  Version: {}", *glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
